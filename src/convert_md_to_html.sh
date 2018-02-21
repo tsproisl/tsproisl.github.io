5e89846d92872e4953b26d590edc12e358477ecd
Thomas Proisl <thomas.proisl@fau.de>
2018-02-21 19:28:29 +0100

    BibTeX and dynamic footer

--- src/convert_md_to_html.sh
+++ src/convert_md_to_html.sh
@@ -2,13 +2,16 @@
 
 BEFORE_BODY=navigation.html
 AFTER_BODY=footer.html
-TODAY=$(date --iso-8601=date)
-
-echo -e "<footer>\n  Last modified: $TODAY.\n</footer>" > "$AFTER_BODY"
 
 for f in *.md
 do
     name=$(basename "$f" .md)
+    TIMESTAMP=$(git log -1 --format=%aI "$f" | sed -re 's/T.+$//')
+    GITDIFF=$(git diff --exit-code --name-only "$f")
+    if [ ! $? -eq 0 ]; then
+	TIMESTAMP=$(date --iso-8601=date -r $f)
+    fi
+    echo -e "<footer>\n  Last modified: $TIMESTAMP.\n</footer>" > "$AFTER_BODY"
     pandoc -f markdown -t html5 --filter pandoc-citeproc --standalone --include-before-body "$BEFORE_BODY" --include-after-body "$AFTER_BODY" --css proisl.css -o ../"$name".html "$f"
 done
 
